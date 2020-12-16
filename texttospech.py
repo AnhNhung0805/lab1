8d067464c8bda91995dfa46c259a56253ed6ddd3
AnhNhung0805 <65988480+AnhNhung0805@users.noreply.github.com>
2020-12-16 15:50:24 +0700

    Update and rename index.php to texttospech.py

---
+++ texttospech.py
@@ -0,0 +1,15 @@
+1.
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(input())
+engine.runAndWait()
+#Engine say when people input text in command
+
+
+
+2.
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hello")
+engine.runAndWait()
+#Engine will say when people run code
